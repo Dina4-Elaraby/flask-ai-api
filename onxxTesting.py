d7ee248ac3b8c6fa885aad194b2fc2ab0aa38355
dinaelaraby <dinaalaraby9503@gmail.com>
2025-06-14 00:38:59 +0300

    OnxxTesting03

--- onxxTesting.py
+++ onxxTesting.py
@@ -188,6 +188,7 @@ for image_file in image_files:
                 'diagnosis': diagnosis,
                
         })
-       
-        if __name__ == '__main__':
-           app.run(debug=True)
+            
+        if __name__ == "__main__":
+            port = int(os.environ.get("PORT", 5000))
+            app.run(host='0.0.0.0', port=port)
